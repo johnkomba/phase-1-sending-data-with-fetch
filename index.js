90e4e23290c5de22487f49e8cb8ebe129e531e43
John Komba <john.mwangi1@student.moringaschool.com>
2023-10-03 14:39:33 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const submitData = {
+    Name: "",
+    email: "",
+  };
+
+const configurationObject = {
+ method: "POST",
+ headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json" ,
+ }  ,
+ body: JSON.stringify(submitData),
+};
+
+return fetch ("http://localhost:4000/users", confifurationObject)
+.then(res => res.json())
+.then(data => {
+    const idElement = document.createElement('p');
+    idElement.textContent = submitData.id;
+    document.body.appendChild(idElement);
+})
+.catch(error => {
+    console.error('Error:', error);
+  });
